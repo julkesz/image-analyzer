90c519068b85ffd435242c53517cf333b34203fb
jszymane <j.szymanek@stud.elka.pw.edu.pl>
2024-02-06 23:42:05 +0100

    Delete example files

--- examples/fraud-example/src/main/java/com/ersms/fraud/FraudCheckHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ersms.fraud;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Integer> {
-
-}
--- examples/fraud-example/src/main/java/com/ersms/fraud/FraudCheckResponse.java
+++
@@ -1,4 +0,0 @@
-package com.ersms.fraud;
-
-public record FraudCheckResponse(Boolean isFraudster) {
-}
